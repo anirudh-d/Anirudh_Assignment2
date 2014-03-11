dd9db3e11168e67a81c50d27147f5086d0100b9d
anirudh-d <ad2930@nyu.edu>
2014-03-11 14:07:41 -0400

    Initial Commit

---
+++ Anirudh_Assignment2/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Anirudh_Assignment2
+//
+
+//  Copyright (c) 2014 Anirudh Donakonda. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Anirudh_Assignment2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Anirudh_Assignment2
+//
+//  Created by Anirudh Donakonda on 3/11/14.
+//  Copyright (c) 2014 Anirudh Donakonda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
